7bb7e00343685742cb3cc49ac508b783722e3303
Steven Macías <ed.steven.macias@gmail.com>
2019-12-03 19:00:27 +0100

    Revert "Merge branch 'develop' into feature/gauges"
    This reverts commit 9a0799226e448d3a93e78bba32e74d65b5cf011a.

--- tools/virtual_port_simulator/virtual_port_simulator.py
+++ tools/virtual_port_simulator/virtual_port_simulator.py
@@ -13,65 +13,28 @@ pressure_max_val = 20.00
 temp_max_val = 200.00
 current_temp = 0.0
 current_press = 0.0
-heater = False
-water_pump = False
-valve_0 = False
-
-def sendJsonString(ser):
-    global pressure_max_val
-    global temp_max_val
-    global current_temp
-    global current_press
-    global heater
-    global water_pump
-    global valve_0
-    ser.write('{"press_sensor_1":'+str(current_press)+', "temp_sensor_1":'+str(current_temp)+', "press_sensor_2":'+str(current_press)+', "temp_sensor_2":'+str(current_temp)+', "heater":'+str(heater)+', "water_pump":'+str(water_pump)+', "valve_0":'+str(valve_0)+', "temp_target":70, "valve_state_1":0, "uControllerState":0}\n')
-    print('{"press_sensor_1":'+str(current_press)+', "temp_sensor_1":'+str(current_temp)+', "press_sensor_2":'+str(current_press)+', "temp_sensor_2":'+str(current_temp)+', "heater":'+str(heater)+', "water_pump":'+str(water_pump)+', "valve_0":'+str(valve_0)+', "temp_target":70, "valve_state_1":0, "uControllerState":0}\n')
-    ser.flush()
-    time.sleep(0.01)
 
 def test_serial():
-    global pressure_max_val
-    global temp_max_val
-    global current_temp
-    global current_press
-    global heater
-    global water_pump
-    global valve_0
     #open a pySerial connection to the slave
-    ser = Serial("/dev/pts/3", 9600, timeout=1)
+    ser = Serial("/dev/pts/2", 9600, timeout=1)
 
     time.sleep(1)
+
     while(True):
-        sendJsonString(ser)
-        valve_0 = True
-        sendJsonString(ser)
-        time.sleep(2)
-        water_pump = True
-        sendJsonString(ser)
-        time.sleep(2)
-        water_pump = False
-        sendJsonString(ser)
-        time.sleep(2)
-        valve_0 = False
-        sendJsonString(ser)
-        time.sleep(2)
-        heater = True
-        sendJsonString(ser)
-        time.sleep(2)
         for x in range(255):
             current_temp = (temp_max_val/255.00)*x
             current_press = (pressure_max_val/255.00)*x
-            sendJsonString(ser)
-        heater = False
-        sendJsonString(ser)
-        time.sleep(2)
-        valve_0 = True
-        sendJsonString(ser)
+            ser.write('{"press_sensor_1":'+str(current_press)+', "temp_sensor_1":'+str(current_temp)+', "valve_state_1":0, "uControllerState":0}\n') #write the first command
+            print('{"press_sensor_1":'+str(current_press)+', "temp_sensor_1":'+str(current_temp)+', "valve_state_1":0, "uControllerState":0}\n')
+            ser.flush()
+            time.sleep(0.01)
         for x in range(255):
             current_temp = temp_max_val-((temp_max_val/255.00)*x)
             current_press = pressure_max_val-((pressure_max_val/255.00)*x)
-            sendJsonString(ser)
+            ser.write('{"press_sensor_1":'+str(current_press)+', "temp_sensor_1":'+str(current_temp)+', "valve_state_1":0, "uControllerState":0}\n') #write the first command
+            print('{"press_sensor_1":'+str(current_press)+', "temp_sensor_1":'+str(current_temp)+', "valve_state_1":0, "uControllerState":0}\n')
+            ser.flush()
+            time.sleep(0.01)
 
 
 if __name__=='__main__':
